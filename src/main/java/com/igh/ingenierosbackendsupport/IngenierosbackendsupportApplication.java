179cfc78d21175dd987ab229ef2f0a06c9158970
marx <salimn78@gmail.com>
2022-01-07 16:27:34 -0500

    start proyect

---
+++ src/main/java/com/igh/ingenierosbackendsupport/IngenierosbackendsupportApplication.java
@@ -0,0 +1,13 @@
+package com.igh.ingenierosbackendsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngenierosbackendsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngenierosbackendsupportApplication.class, args);
+	}
+
+}
